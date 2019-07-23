3b866c2345cbf823a75971e0a33300d9b8f7a78f
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-07-23 17:41:44 +0200

    you

--- experiments/reo.R
+++ experiments/reo.R
@@ -11,8 +11,13 @@ mypng <- function(filename) {
   )
 }
 milis = 1000
+thou = 1000
 bil = 1000000000
 #####################################################################
+### RTT. measuring Reo's fifo1 on a single thread vs mpsc
+### x represents changing data size in bytes (0,1,2,4,8,16)  x gives q where bytes=q&2-1
+### 100_000 data movements
+### 10 reps with fresh proto objects
 
 x = 0:14
 reps = 100000
@@ -37,4 +42,85 @@ mypng("rtt_1.png")
 op <-par(mar = c(5.1, 4.1, 1.8, 1.8))
 plot(x, reo/reps/milis*bil - mpsc/reps/milis*bil, log='', type='l',
      xlab="q for type size = 2^q-1", ylim=c(1, 3010), ylab = "fifo1 RTT overhead (ns)")
+dev.off()
+
+
+#########################################################
+### "SIMO" group getting. N-way replicator with COPY type data
+### x represents changing data size in bytes (0,1,2,4,8,16)  x gives q where bytes=q&2-1
+### 25_000 data movements
+### 100 reps with fresh proto objects
+
+x = c(0,4,8,12,16);
+
+
+getters1 = c(532, 544, 570, 990, 6241);
+getters2 = c(941, 979, 1028, 1805, 9336);
+getters3 = c(1712, 1678, 1755, 2534, 10868);
+getters4 = c(9735, 9792, 9781, 11697, 19656);
+getters5 = c(11339, 10450, 11031, 12465, 25713)
+
+
+mypng("simo_copy_0.png")
+op <- par(mar = c(5.1, 4.1, 1.8, 1.8))
+plot(x, getters1/thou, log='', type='l', xlab="q for type size = 2^q-1",
+     ylim=c(0.3, 26), ylab = "Mean sync time (μs)")
+
+
+lines(x, getters2/thou, type='l', col=2, lty=2)
+lines(x, getters3/thou, type='l', col=3, lty=3)
+lines(x, getters4/thou, type='l', col=4, lty=4)
+lines(x, getters5/thou, type='l', col=5, lty=5)
+legend("topleft", legend=1:5 , col=1:5,  ncol=2,
+       y.intersp=1.3, cex=1, xjust=0, bty = "n", lty=1:5
+)
+dev.off()
+
+
+### now plotted relative to getters=1
+mypng("simo_copy_1.png")
+plot(x, getters2/getters1, log='', type='l', xlab="q for type size = 2^q-1",
+     ylim=c(0.8, 20), ylab = "Ratio vs getters=1", col=2, lty=2)
+
+abline(h=1)
+lines(x, getters3/getters1, type='l', col=3, lty=3)
+lines(x, getters4/getters1, type='l', col=4, lty=4)
+lines(x, getters5/getters1, type='l', col=5, lty=5)
+legend("left", legend=2:5 , col=2:5,  ncol=1,
+       y.intersp=1.3, cex=1, xjust=0, bty = "n", lty=2:5
+)
+dev.off()
+
+
+#########################################################
+### "SIMO cloning" group getting. N-way replicator with CLONE type data
+### x is the time in WORK UNITS for some arbitrary clone operation.
+
+x =        c(4,     8,     16,    32,    64,    128,   256,   512,   1024,  2048,  4096,  8192);
+getters1 = c(697,   633,   663,   671,   683,   678,   667,   797,   926,   820,   940,   745);
+getters2 = c(1872,  1849,  1960,  1883,  2336,  4682,  3626,  8554,  28168, 24925, 52270, 66560);
+getters3 = c(3878,  5019,  4979,  3421,  3161,  4996,  8398,  13045, 18986, 32706, 49233, 77842);
+getters4 = c(15510, 12252, 10923, 12419, 13574, 12961, 13218, 15832, 19988, 33457, 51436, 90750);
+getters5 = c(14513, 13862, 13880, 14020, 15721, 16185, 15658, 18702, 20822, 31647, 55072, 92740);
+
+mypng("clone_compete_0.png")
+plot(x, getters1/thou, log='', type='l', xlab="clone work units",
+     ylim=c(0.5, 100), ylab = "mean sych time (μs)", col=2, lty=2)
+lines(x, getters2/thou, type='l', col=3, lty=3)
+lines(x, getters3/thou, type='l', col=3, lty=3)
+lines(x, getters4/thou, type='l', col=4, lty=4)
+lines(x, getters5/thou, type='l', col=5, lty=5)
+legend("topleft", legend=1:5 , col=1:5,  ncol=2,
+       y.intersp=1.3, cex=1, xjust=0, bty = "n", lty=1:5)
+dev.off()
+
+mypng("clone_compete_1.png")
+plot(x, getters1/thou, log='xy', type='l', xlab="clone work units",
+     ylim=c(0.5, 100), ylab = "mean sych time (μs)", col=3, lty=2)
+lines(x, getters2/thou, type='l', col=3, lty=3)
+lines(x, getters3/thou, type='l', col=3, lty=3)
+lines(x, getters4/thou, type='l', col=4, lty=4)
+lines(x, getters5/thou, type='l', col=5, lty=5)
+legend("topleft", legend=1:5 , col=1:5,  ncol=2,
+       y.intersp=1.3, cex=1, xjust=0, bty = "n", lty=1:5)
 dev.off()
