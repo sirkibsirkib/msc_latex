e11529c3a3ef67a09c5b2347d8eb90b5b453be56
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-07-26 18:05:34 +0200

    you

--- experiments/reo.R
+++ experiments/reo.R
@@ -3,8 +3,8 @@ setwd("C:/Users/chris/OneDrive/Documents/LaTeX/Msc2/experiments")
 mypng <- function(filename) {
   png(
     filename,
-    width     = 3.4,
-    height    = 3.0,
+    width     = 3.1,
+    height    = 2.8,
     units     = "in",
     res       = 1000,
     pointsize = 9
@@ -13,8 +13,8 @@ mypng <- function(filename) {
 mypngbig <- function(filename) {
   png(
     filename,
-    width     = 4.4,
-    height    = 3.5,
+    width     = 4.2,
+    height    = 3.35,
     units     = "in",
     res       = 1000,
     pointsize = 9
@@ -30,20 +30,21 @@ bil = thou*thou*thou
 
 x = c(0, 2^(1:14))
 reps = 100000
-reo = c(63.13461, 65.14954, 66.80122, 63.49879, 64.86871, 66.98967, 69.14193,
-      69.47564, 76.61765, 89.04745, 114.10968, 134.26562,
-      181.08886, 299.73297, 903.00934);
 mpsc = c(9.29991, 10.65254, 10.5035, 10.92912, 10.03323, 10.07968, 11.2787,
          12.20944, 16.24493, 25.9815, 28.36046, 38.37119,
          69.29128, 163.17103, 276.16403);
+reo = c(63.13461, 65.14954, 66.80122, 63.49879, 64.86871, 66.98967, 69.14193,
+      69.47564, 76.61765, 89.04745, 114.10968, 134.26562,
+      181.08886, 299.73297, 903.00934);
 
 mypng("rtt_0.png")
 op <- par(mar = c(5.1, 4.1, 1.5, 1.5))
-dat = matrix(c(reo, mpsc), ncol=2)
+dat = matrix(c(mpsc, reo), ncol=2)
 matplot(x, dat/reps/thou*bil, log='x', type='b', pch=1:2, xlab="data size (bytes)",
      ylim=c(1, 3010), ylab = "Mean RTT (ns)")
-legend("topleft", legend=c("fifo1", "mpsc channel") , col=1:2,  ncol=1, 
-       y.intersp=1.3, cex=1, xjust=0, bty = "n", pch=1:2
+revd=c(2,1)
+legend("topleft", legend=c("fifo1", "mpsc channel") , col=revd,  ncol=1, 
+       y.intersp=1.3, cex=1, xjust=0, bty = "n", pch=revd
 )
 dev.off()
 
@@ -283,9 +284,10 @@ dat <- matrix(c(y_handmade, y_reorsput), ncol=2);
 mypngbig("alternator.png")
 op <- par(mar = c(5.1, 4.1, 1.5, 1.5))
 dat = matrix(c(y_crossbea, y_reorsput), ncol=2)
-matplot(x, dat/thou, type = "b", lty=1:3, col=1:3, log='x', ylab="interaction duration (μs)", xlab="data size (bytes)", pch=1:2, ylim=c(0,8)) #plot
+matplot(x, dat/thou, type = "b", lty=1:2, col=1:2, log='x', ylab="interaction duration (μs)", xlab="data size (bytes)", pch=1:2, ylim=c(0,8)) #plot
 lines(c(1024, 2048, 4096), c(2164, 2941, 6062)/thou, type="b", col=3, pch=3)
-legend("topleft", legend=c("handmade", "reo-rs", "reo-rs (unsafe reference API)") , col=1:3,  ncol=1,
-       y.intersp=1.1, cex=1, xjust=0, bty = "n", pch=1:3)
+revd=c(2,3,1)
+legend("topleft", legend=c("reo-rs", "reo-rs (unsafe reference API)","handmade") , col=revd,  ncol=1,
+       y.intersp=1.1, cex=1, xjust=0, bty = "n", pch=revd)
 dev.off()
 
