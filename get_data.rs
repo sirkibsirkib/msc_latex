89e2507af17e27916923a4811bff6e75fd307d41
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-07-26 17:24:38 +0200

    you

--- get_data.rs
+++ get_data.rs
@@ -1,18 +1,38 @@
-if want_data {
-    let am_mover = mover.swap(false);
-    if am_mover {
-        if count.sub_fetch(1) == 0 { do_move() }
-        else { mover_signal.await() }
-        cleanup(was_moved=true);
+if T::IS_COPY { // irrelevant how many copy
+    if let Some(dest) = maybe_dest {
+        do_move(dest);
+        m.visit();
+    }
+    let was = count.fetch_dec();
+    if was == LAST {
+        let [visited_first, retains] = m.visit();
+        finalize();
+    }
+} else {
+    if let Some(dest) = maybe_dest {
+        let [visited_first, retains] = m.visit();
+        if visited_first && !retains {
+            let was = count.fetch_dec();
+            if was != LAST {
+                mover_await();
+            }
+            do_move(dest);
+            finalize();
+        } else {
+            do_clone(dest);
+            let was = count.fetch_dec();
+            if was == LAST {
+                if retains {
+                    finalize();
+                } else {
+                    mover_release();
+        }   }   }
     } else {
-        do_clone();
-        if count.sub_fetch(1) == 0 {
-            // I was the last cloner. All cloners done.
-            mover_signal.release();
-    }   }
-} else { // want signal only
-    if count.sub_fetch(1) == 0 {
-        let am_mover = mover.swap(false);
-        if am_mover { cleanup(was_moved=false) }
-		else { mover_signal.release() }
-}   }
+        let was = count.fetch_dec();
+        if was == LAST {
+            let [visited_first, retains] = m.visit();
+            if visited_first {
+                finalize();
+            } else {
+                mover_release();
+}   }   }   }
