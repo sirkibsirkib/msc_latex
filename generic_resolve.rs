1020f867822cafd327d6f6af4e0a28114e3f2ff5
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-08-13 17:17:21 +0200

    writing background

--- generic_resolve.rs
+++ generic_resolve.rs
@@ -1,24 +1,21 @@
-fn build_protocol_1<T>() -> Proto {
-where
-	T: Clone,
-{
+fn build_protocol_1<T: Clone>() -> Proto {
     ProtoDef {
-		let type_info = TypeInfo::of::<T>();
+        let type_info = TypeInfo::of::<T>();
         name_defs: hashmap! {
             "P" => Port { is_putter:true, type_info },
             "C0" => Port { is_putter:false, type_info },
             "C1" => Port { is_putter:false, type_info },
         },
         rules: vec![RuleDef {
-			state_guard: StatePredicate {
-				ready_ports: hashset! {"P", "C0", "C1"},
-				full_mem: hashset! {},
-				empty_mem: hashset! {},
-			},
-			ins: vec![],
-			output: hashmap! {
-				"P" => (false, hashset!{"C0", "C1"}),
-			},
-		}],
+            state_guard: StatePredicate {
+                ready_ports: hashset! {"P", "C0", "C1"},
+                full_mem: hashset! {},
+                empty_mem: hashset! {},
+            },
+            ins: vec![],
+            output: hashmap! {
+                "P" => (false, hashset!{"C0", "C1"}),
+            },
+        }],
     }.build(MemInitial::default()).unwrap()
 }
