a5206ae8b0571a0eb24a22b85582b97737a0acd5
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-08-15 19:43:10 +0200

    hyphen tasticc

--- move.rs
+++ move.rs
@@ -1,9 +1,9 @@
 struct Foo;
-fn func(x: Foo) {
-	// this function takes argument `x` by value.
-}
+fn func(x: Foo) { // this function moves its argument into its scope (by value).
+}                 // `x` goes out of scope and is (trivially) dropped.
+
 fn main(){
-	let x = Foo; // instantiate.
-	func(x);     // Ok. `x` is moved into `func`.
-	func(x);     // Error! x is used after move.
+    let x = Foo;  // (trivial) initialization.
+    func(x);      // Ok. `x` is moved into `func`.
+    func(x);      // Error! x is used after move.
 }
